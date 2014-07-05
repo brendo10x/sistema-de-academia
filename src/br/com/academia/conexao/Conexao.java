d229c4173432b000442fb5a8f338d4f56b3abe00
brendo10x <brendo10x@gmail.com>
2014-07-05 11:53:37 -0300

    Aqui é o meu comemtário de teste

--- src/br/com/academia/conexao/Conexao.java
+++ src/br/com/academia/conexao/Conexao.java
@@ -1,5 +1,7 @@
 package br.com.academia.conexao;
 
+///Aqui weslly alguma alteração
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
