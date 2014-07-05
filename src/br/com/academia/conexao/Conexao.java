9c6a1a9727f99f209b378516d2ef71e9016a8401
brendo10x <brendo10x@gmail.com>
2014-07-05 12:46:54 -0300

    Atualização 05/07/2014 - 12:46

--- src/br/com/academia/conexao/Conexao.java
+++ src/br/com/academia/conexao/Conexao.java
@@ -1,5 +1,7 @@
 package br.com.academia.conexao;
 
+//Alterei aqui e no de weslly
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
